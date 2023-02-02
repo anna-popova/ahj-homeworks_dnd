de67368f3f21837a9ea787d3c7432a90720f8155
Anna Popova <panasenko-anna.8@mail.ru>
2023-02-02 10:02:45 +0300

    добавляет функционал по добавлению новых карточек

--- src/js/app.js
+++ src/js/app.js
@@ -1,101 +1,133 @@
-const cards = document.querySelectorAll('.card');
-const addCardLinks = document.querySelectorAll('.add-card-link');
+const container = document.querySelector('.container');
+const addCardLinks = Array.from(container.querySelectorAll('.add-card-link'));
+//console.log(addCardLinks);
+const lists = Array.from(container.querySelectorAll('.list'));
+//console.log(lists);
 
-//!добавление карточки
+const allCards = [];
+const todoCards = [];
+const progressCards = [];
+const doneCards = [];
+
+//!Серверную часть и загрузку также реализовывать не нужно, храните всё в памяти
+function save(arr) {
+	localStorage.editorData = JSON.stringify({
+		arr,
+	});
+}
+
+function restore() {
+	const json = localStorage.editorData;
+
+	if (!json) {
+		return;
+	}
+
+	const data = JSON.parse(json);
+
+	console.log(data.arr);
+	console.log(data.arr.length);
+
+  //TODO: stop here. Достать данные из массива из LocalStogare и добавить их на страницу
+
+	// for (let i = 0; i < data.arr.length; i++) {
+	// 	//console.log(data.arr[i]);
+
+	// 	const li = document.createElement('li');
+	// 	li.classList.add('post');
+	// 	li.innerHTML = `
+	// 	<div class="post-header">
+	// 		<span class="date">${data.arr[i].day}.${data.arr[i].month}.${data.arr[i].year}</span>
+	// 		<span class="time">${data.arr[i].hours}:${data.arr[i].minutes}</span>
+	// 	</div>
+	// 	<p>${data.arr[i].text}</p>
+	// 	<span class="geolocation">[${data.arr[i].latitude}, ${data.arr[i].longitude}]</span>
+	// 	`;
+
+	// 	postsList.prepend(li);
+
+	// 	doneCards.push(data.arr[i]);
+	// }
+}
+
+window.onload = function() {
+	console.log(doneCards);
+	restore();
+};
+
+//!добавляем новую карточку в список
 for (const link of addCardLinks) {
   link.addEventListener('click', (e) => {
     e.preventDefault();
 
-    const addCardSection = link.parentElement;
-    //console.log(addCardSection);
+    const parentList = link.closest('.list');
+    console.log(parentList);
+    const cardList = parentList.querySelector('.card-list');
+    //console.log(cardList);
+    const textarea = parentList.querySelector('.textarea');
 
-    const addCardAction = addCardSection.querySelector('.add-card-action');
-    const addCardText = addCardSection.querySelector('.add-card-text');
-    const addCardButton = addCardSection.querySelector('.add-card-button');
-    const cancelCardButton = addCardSection.querySelector('.cancel-card-button');
-
+    const addCardAction = parentList.querySelector('.add-card-action');
     addCardAction.classList.remove('visually-hidden');
-    link.classList.add('visually-hidden');
 
-    addCardButton.addEventListener('click', () => {
-      //скрываем текстовое поле с кнопками
-      addCardAction.classList.add('visually-hidden');
-      link.classList.remove('visually-hidden');
-
-      //создаем новый элемент
-      const li = document.createElement('li');
-      li.className = 'card';
-      li.innerHTML = `${addCardText.value}<button class="delete-button" type="button"></button>`;
-
-      //вставляем новый элемент в соответствующий список
-      link.before(li);
-
-      //очищаем форму
-      addCardText.value = '';
-    });
+    const cancelCardButton = parentList.querySelector('.cancel-card-button');
 
     cancelCardButton.addEventListener('click', () => {
       addCardAction.classList.add('visually-hidden');
-      link.classList.remove('visually-hidden');
+      textarea.value = '';
+    });
+
+    const addCardButton = parentList.querySelector('.add-card-button');
+
+    addCardButton.addEventListener('click', () => {
+      if (textarea.value !== '') {
+        const postText = textarea.value;
+        //console.log(postText);
+
+        addNewCard(postText, cardList);
+
+        addCardsToArray(parentList, postText);
+
+        save(allCards);
+
+        textarea.value = '';
+      };
     });
   });
-};
+}
 
-//!удаление карточки
-for (const card of cards) {
-  card.addEventListener('mouseover', () => {
-    const deleteButton = card.querySelector('.delete-button');
-    deleteButton.classList.add('active');
+function addNewCard(text, parentList) {
+  const li = document.createElement('li');
+  li.className = 'card';
+  li.innerHTML = `<p>${text}</p>
+                  <button class="delete-button" type="button"></button>`;
+  parentList.prepend(li);
+}
 
-    deleteButton.addEventListener('click', () => {
-      card.remove();
+function addCardsToArray(list, text) {
+  if (list.classList.contains('todo-list')) {
+    todoCards.push({
+      text,
     })
-  });
-
-  card.addEventListener("mouseout", () => {
-    const deleteButton = card.querySelector('.delete-button');
-    deleteButton.classList.remove('active');
-  });
-};
-
-//!перемещение карточки
-const lists = document.querySelectorAll('.list');
-const card = document.querySelector('.card');
-let actualElement;
-
-const onMouseOver = (e) => {
-  const closest = document.elementFromPoint(e.clientX, e.clientY);
-  const { top } = closest.getBoundingClientRect();
-
-  if (closest.classList.contains('card')) {
-    if (e.pageY > window.scrollY + top + closest.offsetHeight / 2) {
-      closest.closest('.list').insertBefore(actualElement, closest.nextElementSibling);
-    } else {
-      closest.closest('.list').insertBefore(actualElement, closest);
-    }
-  } else if (closest.classList.contains('list')) {
-    closest.append(actualElement);
+  } else if (list.classList.contains('progress-list')) {
+    progressCards.push({
+      text,
+    })
+  } else {
+    doneCards.push({
+      text,
+    })
   }
-};
 
-const onMouseUp = (e) => {
-  actualElement.classList.remove('gragged');
+  // console.log(todoCards);
+  // console.log(progressCards);
+  // console.log(doneCards);
 
-  actualElement = undefined;
+  allCards.push({
+    todoCards: todoCards,
+    progressCards: progressCards,
+    doneCards: doneCards,
+  })
 
-  document.documentElement.removeEventListener('mouseup', onMouseUp);
-  document.documentElement.removeEventListener('mouseover', onMouseOver);
-};
+  //console.log(allCards);
+}
 
-for (const list of lists) {
-  list.addEventListener('mousedown', (e) => {
-    e.preventDefault();
-
-    actualElement = e.target;
-    
-    actualElement.classList.add('gragged');
-
-    document.documentElement.addEventListener('mouseup', onMouseUp);
-    document.documentElement.addEventListener('mouseover', onMouseOver);
-  });
-};
