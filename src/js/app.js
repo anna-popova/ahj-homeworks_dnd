8263c6a295737144e667cebe2c569be4a1e0e806
Anna Popova <panasenko-anna.8@mail.ru>
2023-02-02 10:57:30 +0300

    добавляет схранение данных при перезагрузке страницы

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ const lists = Array.from(container.querySelectorAll('.list'));
 //console.log(lists);
 
 const allCards = [];
+
 const todoCards = [];
 const progressCards = [];
 const doneCards = [];
@@ -25,33 +26,55 @@ function restore() {
 
 	const data = JSON.parse(json);
 
-	console.log(data.arr);
-	console.log(data.arr.length);
+	//console.log(data.arr);
+  const todoCardsLength = data.arr[0].todoCards.length;
+	//console.log(todoCardsLength);
+  const progressCardsLength = data.arr[0].progressCards.length;
+  //console.log(progressCardsLength);
+  const doneCardsLength = data.arr[0].doneCards.length;
+  //console.log(doneCardsLength);
 
-  //TODO: stop here. Достать данные из массива из LocalStogare и добавить их на страницу
+  //TODO: stop here.
 
-	// for (let i = 0; i < data.arr.length; i++) {
-	// 	//console.log(data.arr[i]);
+	for (let i = 0; i < todoCardsLength; i++) {
+    //console.log(data.arr[0].todoCards[i].text);
 
-	// 	const li = document.createElement('li');
-	// 	li.classList.add('post');
-	// 	li.innerHTML = `
-	// 	<div class="post-header">
-	// 		<span class="date">${data.arr[i].day}.${data.arr[i].month}.${data.arr[i].year}</span>
-	// 		<span class="time">${data.arr[i].hours}:${data.arr[i].minutes}</span>
-	// 	</div>
-	// 	<p>${data.arr[i].text}</p>
-	// 	<span class="geolocation">[${data.arr[i].latitude}, ${data.arr[i].longitude}]</span>
-	// 	`;
+    const todoCardList = document.querySelector('.todo-card-list');
 
-	// 	postsList.prepend(li);
+    addNewCard(data.arr[0].todoCards[i].text, todoCardList);
 
-	// 	doneCards.push(data.arr[i]);
-	// }
+    todoCards.push({
+      text: data.arr[0].todoCards[i].text,
+    })
+  }
+
+  for (let i = 0; i < progressCardsLength; i++) {
+    //console.log(data.arr[0].progressCards[i].text);
+
+    const progressCardList = document.querySelector('.progress-card-list');
+
+    addNewCard(data.arr[0].progressCards[i].text, progressCardList);
+
+    progressCards.push({
+      text: data.arr[0].progressCards[i].text,
+    })
+  }
+
+  for (let i = 0; i < doneCardsLength; i++) {
+    //console.log(data.arr[0].doneCards[i].text);
+
+    const doneCardList = document.querySelector('.done-card-list');
+
+    addNewCard(data.arr[0].doneCards[i].text, doneCardList);
+
+    doneCards.push({
+      text: data.arr[0].doneCards[i].text,
+    })
+  }
 }
 
 window.onload = function() {
-	console.log(doneCards);
+	//console.log(doneCards);
 	restore();
 };
 
@@ -90,10 +113,20 @@ for (const link of addCardLinks) {
         save(allCards);
 
         textarea.value = '';
+
+        addCardAction.classList.add('visually-hidden');
       };
     });
   });
 }
+
+allCards.push({
+  todoCards: todoCards,
+  progressCards: progressCards,
+  doneCards: doneCards,
+})
+
+//console.log(allCards);
 
 function addNewCard(text, parentList) {
   const li = document.createElement('li');
@@ -121,13 +154,5 @@ function addCardsToArray(list, text) {
   // console.log(todoCards);
   // console.log(progressCards);
   // console.log(doneCards);
-
-  allCards.push({
-    todoCards: todoCards,
-    progressCards: progressCards,
-    doneCards: doneCards,
-  })
-
-  //console.log(allCards);
 }
 
